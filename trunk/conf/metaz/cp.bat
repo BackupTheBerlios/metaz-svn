242006ab57bd115a67cc380842e3206e22010889
loosterloo <loosterloo@fa9a27b5-f702-0410-86e9-f74c14d606bd>
2005-12-28 22:08:14 +0000

    replaces setup.bat

---
+++ trunk/conf/metaz/cp.bat
@@ -0,0 +1,10 @@
+REM FP: Huidige directory
+SET CLASSPATH=.
+
+REM FP: Source root
+SET CLASSPATH=%CLASSPATH%;%METAZHOME%\src
+
+REM FP: Library directory
+SET CLASSPATH=%CLASSPATH%;%METAZHOME%\lib
+
+REM Toevoegingen hieronder opnemen met initialen!
