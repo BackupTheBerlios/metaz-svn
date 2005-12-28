64f78418c026d5b5b3ca568e84fb2c56e8585dea
ejspaans <ejspaans@fa9a27b5-f702-0410-86e9-f74c14d606bd>
2005-12-28 22:58:40 +0000

    Initial version of helloworld project. Meant to get used to maven.

---
+++ trunk/helloworld/src/test/java/org/metaz/helloworld/HelloWorldTest.java
@@ -0,0 +1,16 @@
+package org.metaz.helloworld;
+
+import junit.framework.TestCase;
+
+public class HelloWorldTest extends TestCase {
+
+	/*
+	 * Test method for 'org.metaz.helloworld.HelloWorld.main(String[])'
+	 * Nonsense test to demonstrate test goal in Maven.
+	 */
+	public void testMain() {
+		HelloWorld.main(null);
+		assertTrue(true);
+	}
+
+}
