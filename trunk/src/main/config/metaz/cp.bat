31c5e9b25bfb7fa10c6bf229f1949c84fc42b4e9
ejspaans <ejspaans@fa9a27b5-f702-0410-86e9-f74c14d606bd>
2005-12-30 09:50:37 +0000

    Copied remotely

---
+++ trunk/src/main/config/metaz/cp.bat
@@ -0,0 +1,10 @@
+REM FP: Huidige directory
+SET CLASSPATH=.
+
+REM LO: Source root
+SET CLASSPATH=%CLASSPATH%;%METAZHOME%\src\main\java
+
+REM LO: Library directory
+SET CLASSPATH=%CLASSPATH%;%METAZHOME%\src\main\resources
+
+REM Toevoegingen hieronder opnemen met initialen!
