70a3cccf2280753911421fb8c42a6fb01fa046cc
ejspaans <ejspaans@fa9a27b5-f702-0410-86e9-f74c14d606bd>
2006-01-08 20:32:36 +0000

    Regenerated as a result of different repository paths.

--- trunk/src/main/config/metaz/autogeneratedcp.bat
+++ trunk/src/main/config/metaz/autogeneratedcp.bat
@@ -1,1 +1,1 @@
-SET CLASSPATH=%CLASSPATH%;%MAVEN_HOME_LOCAL%\repository\junit3.8.1.jar;%MAVEN_HOME_LOCAL%\repository\log4j1.2.8.jar;%MAVEN_HOME_LOCAL%\repository\lucene1.4.3.jar;%MAVEN_HOME_LOCAL%\repository\hsqldb1.8.0.1.jar;
+SET CLASSPATH=%CLASSPATH%;%MAVEN_HOME_LOCAL%\repository\junit\jars\junit3.8.1.jar;%MAVEN_HOME_LOCAL%\repository\log4j\jars\log4j1.2.8.jar;%MAVEN_HOME_LOCAL%\repository\lucene\jars\lucene1.4.3.jar;%MAVEN_HOME_LOCAL%\repository\hsqldb\jars\hsqldb1.8.0.1.jar;
