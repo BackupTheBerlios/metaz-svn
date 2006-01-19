6dd1bdd4506cb8afaca84c0ec0d6acb4b0b7d187
loosterloo <loosterloo@fa9a27b5-f702-0410-86e9-f74c14d606bd>
2006-01-19 19:05:53 +0000

    not fully functional due to missing library

--- trunk/src/main/java/org/metaz/harvester/Harvester.java
+++ trunk/src/main/java/org/metaz/harvester/Harvester.java
@@ -4,14 +4,14 @@ import org.apache.log4j.Logger;
 import org.metaz.util.MetaZ;
 
 import java.io.*;
-//import java.util.*;
+import java.util.*;
 
 import org.dom4j.Document;
-//import org.dom4j.Element;
+import org.dom4j.Element;
 import org.dom4j.DocumentException;
 import org.dom4j.io.SAXReader;
 import org.dom4j.io.SAXWriter;
-//import org.dom4j.io.DOMReader;
+import org.dom4j.io.DOMReader;
 
 import org.iso_relax.verifier.Schema;
 import org.iso_relax.verifier.Verifier;
@@ -37,21 +37,26 @@ import com.sun.msv.verifier.jarv.TheFactoryImpl;
 public class Harvester {
 	
 	private static Logger logger = MetaZ.getLogger(Harvester.class);
-	private final static String APPLICATIONZ_SCHEMA="metaz.xsd";
-	protected String xmlfile;
+	private final static String APPLICATIONZ_SCHEMA="xml/schema/metaz.xsd";
+	private final static String APPLICATIONZ_TRANSFER_PATH="xml/transfer";
+	private final static String APPLICATIONZ_PROCESSED_PATH="xml/log/processed";
+	private final static String APPLICATIONZ_REJECTED_PATH="xml/log/error";
+	private final static String APPLICATIONZ_TRANSFERSTAGING_PATH="xml/transferstaging";
+	protected File xmlfile;
 	
 	/**
 	 * @param args arguments, first one should contain xml file to parse
 	 */
 	public static void main(String[] args) {
-
+		String filename="koppeling.xml";
 		if (args.length==1){
-			//initiate the Harvester class
-			Harvester harvester = new Harvester();
+			
 			//parse filename from args
-			String filename=args[0].toString();
-			harvester.setXMLFile(filename);
+			filename=args[0].toString();
 		}
+		//initiate the Harvester class
+		Harvester harvester = new Harvester();
+		harvester.setXMLFile(filename);
 	}
 	
 	/**
@@ -67,43 +72,79 @@ public class Harvester {
 	 * @param f
 	 */
 	protected void setXMLFile(String f){
-		xmlfile = f;
-		parseFile(xmlfile);
+		//copy file to gain exclsive rights
+		MetaZ app = MetaZ.getInstance();
+		try{
+			File xmlfile = app.getRelativeFile(APPLICATIONZ_TRANSFER_PATH+ "/" + f);
+			logger.info(xmlfile.getAbsoluteFile().toString());
+			if (xmlfile.canWrite()){
+				xmlfile.renameTo(app.getRelativeFile(APPLICATIONZ_TRANSFERSTAGING_PATH+ "/" + f));
+				logger.info("after rename" + xmlfile.getAbsolutePath());
+				xmlfile = app.getRelativeFile(APPLICATIONZ_TRANSFERSTAGING_PATH + "/" + f);
+				parseFile(xmlfile);
+				logger.info("after parsing" + xmlfile.getAbsolutePath());
+				long timestamp = System.currentTimeMillis();
+				xmlfile.renameTo(app.getRelativeFile(APPLICATIONZ_PROCESSED_PATH+ "/" + Long.toString(timestamp) + f));
+			}
+			else {
+				logger.fatal("The file " + xmlfile.getName() + " was locked!");
+			}
+		}
+		catch (Exception ex){
+			logger.error(ex.getMessage());
+		}
+		
 	}
 	
 	/**
 	 * 
 	 * @param f
 	 */
-	private void parseFile(String f){
+	private boolean parseFile(File f){
 		try{
 		//validate and create document
-		//Document doc = 
-			getDom4jDocument(f);
+			logger.info("start parsing");
+		Document doc = getDom4jDocument(f);
+		logger.info("doc created");
 		//convert to collection of LearningObjects
+		//test
+		Element root = doc.getRootElement();
+		// iterate through child elements of root
+		for ( Iterator i = root.elementIterator(); i.hasNext(); )
+		{
+			Element element = (Element) i.next();
+			// do something
+			System.out.println("Element Name:"+element.getQualifiedName() );
+			System.out.println("Element Value:"+element.getText());
+		}
 		//iterate throught the xml
 		//add every learnobject to the collection
 		//release file
+		
 		//call repository interface with collection
+			return true;
 		}
 		catch (Exception ex){
 			logger.error(ex.getMessage());
 		}
+		return false;
 	}
 	
 /**
  * Converts a file in the filesystem into a DOM4J document
- * ToDo: add validation against a schema
+ * 
  * @param f filename of the file to convert to a DOM4J document
  * @return the DOM4J document to return, null if an error occurs
  */
-	private Document getDom4jDocument(String f) throws Exception{
+	private Document getDom4jDocument(File f) throws Exception{
 		{
 			Document document=null;
 			try{
 				SAXReader reader = new SAXReader();
-				document = reader.read(new File(f));
-				processValidation(document);			
+				document = reader.read(f);
+				logger.info("doc read");
+				processValidation(document);
+				logger.info("document created and validated");
 			}
 			catch (DocumentException ex){
 				logger.error(ex.getMessage());
@@ -126,14 +167,15 @@ public class Harvester {
  * @throws Exception
  */	
     protected void processValidation(Document document) throws Exception {
-
-        logger.info( "Loaded schema document: " + APPLICATIONZ_SCHEMA );
-        
+    	MetaZ app = MetaZ.getInstance();
+        logger.info( "Loaded schema document: " + APPLICATIONZ_SCHEMA ); 
         // use autodetection of schemas
         VerifierFactory factory = new TheFactoryImpl();
-        Schema schema = factory.compileSchema( APPLICATIONZ_SCHEMA );
-                  
+        logger.info("after validation factory");
+        Schema schema = factory.compileSchema( app.getRelativeFile(APPLICATIONZ_SCHEMA));
+        logger.info("schema loaded");          
         Verifier verifier = schema.newVerifier();
+        logger.info("verifier created");
         verifier.setErrorHandler(
             new ErrorHandler() {
                 public void error(SAXParseException e) {
