bc42d6a7a89a4d9730e4e4f53cca8a8c78ef7e50
erik-metaz <erik-metaz@fa9a27b5-f702-0410-86e9-f74c14d606bd>
2006-01-22 21:13:53 +0000

    Nieuwe versie CLI to do: - gebruik MetaData klasse - Javadoc - controle input - makkelijke mogelijkheid om zoekstring in te geven. Met nummers refererent aan omschrijvingen? - koppeling met repository

--- trunk/src/main/java/org/metaz/gui/PrototypeCLI.java
+++ trunk/src/main/java/org/metaz/gui/PrototypeCLI.java
@@ -2,41 +2,80 @@ package org.metaz.gui;
 
 import org.apache.commons.cli.*;
 
+import org.metaz.repository.SearchServiceImpl;
+
+import java.util.List;
+
+
 public class PrototypeCLI {
     public PrototypeCLI() {
     }
 
     public static void main(String[] args) {
-        PrototypeCLI prototypeCLI = new PrototypeCLI();
+        //PrototypeCLI prototypeCLI = new PrototypeCLI();
 
         Options options = new Options();
 
-        Option idOpt =
-            new Option("id", "identity", false, "Identity(Docent|Begeleider|Manager)");
-        idOpt.setArgs(1);
-        idOpt.setType(new String());
-        idOpt.setRequired(false);
-        options.addOption(idOpt);
+        Option gOpt =
+            new Option("g", "gebruiker", false, "Gebruiker(Docent|Begeleider|Manager)");
+        gOpt.setArgs(1);
+        gOpt.setType(new String());
+        gOpt.setRequired(false);
+        options.addOption(gOpt);
 
-        Option schoolOpt =
-            new Option("sc", "school", false, "School Type(Pre-primair onderwijs|Primair onderwijs|Voortgezet onderwijs)");
-        schoolOpt.setArgs(1);
-        schoolOpt.setType(new String());
-        schoolOpt.setRequired(false);
-        options.addOption(schoolOpt);
+        Option sOpt =
+            new Option("s", "schooltype", false, "Schooltype(Pre-primair onderwijs|Primair onderwijs|Voortgezet onderwijs)");
+        sOpt.setArgs(1);
+        sOpt.setType(new String());
+        sOpt.setRequired(false);
+        options.addOption(sOpt);
 
-        Option searchOpt = new Option("s", "search", false, "Search string");
-        searchOpt.setArgs(1);
-        searchOpt.setType(new String());
-        searchOpt.setRequired(false);
-        options.addOption(searchOpt);
+        Option vOpt =
+            new Option("v", "vakleergebied", false, "Vakleergebied(Toepasselijke vakkenlijst)");
+        vOpt.setArgs(1);
+        vOpt.setType(new String());
+        vOpt.setRequired(false);
+        options.addOption(vOpt);
 
-        Option helpOpt =
-            new Option("h", "help", false, "Shows the help message");
-        helpOpt.setArgs(0);
-        helpOpt.setType(new String());
-        helpOpt.setRequired(false);
-        options.addOption(helpOpt);
+        Option dOpt =
+            new Option("d", "didactischeFunctie", false, "Didactische Functie(Oefening, Simulatie, enz)");
+        dOpt.setArgs(1);
+        dOpt.setType(new String());
+        dOpt.setRequired(false);
+        options.addOption(dOpt);
+
+        Option pOpt =
+            new Option("p", "producttype", false, "Producttype(Document, Afbeelding, Video, enz)");
+        pOpt.setArgs(1);
+        pOpt.setType(new String());
+        pOpt.setRequired(false);
+        options.addOption(pOpt);
+
+        Option bOpt =
+            new Option("b", "beroepssituatie", false, "Beroepssituatie(...een zeer lange lijst...)");
+        bOpt.setArgs(1);
+        bOpt.setType(new String());
+        bOpt.setRequired(false);
+        options.addOption(bOpt);
+
+        Option cOpt =
+            new Option("c", "competentie", false, "Competentie(Interpersoonlijk, Pedagogisch, enz...)");
+        cOpt.setArgs(1);
+        cOpt.setType(new String());
+        cOpt.setRequired(false);
+        options.addOption(cOpt);
+
+        Option tOpt = new Option("t", "trefwoorden", false, "Trefwoorden");
+        tOpt.setArgs(0);
+        tOpt.setType(new String());
+        tOpt.setRequired(false);
+        options.addOption(tOpt);
+
+        Option hOpt = new Option("h", "help", false, "Laat de helptekst zien");
+        hOpt.setArgs(0);
+        hOpt.setType(new String());
+        hOpt.setRequired(false);
+        options.addOption(hOpt);
 
         BasicParser parser = new BasicParser();
         CommandLine cmd = null;
@@ -51,23 +90,60 @@ public class PrototypeCLI {
             java.lang.System.exit(0);
         }
 
-        String optIdentity = cmd.getOptionValue(idOpt.getOpt());
-        String optSchool = cmd.getOptionValue(schoolOpt.getOpt());
-        String optSearch = cmd.getOptionValue(searchOpt.getOpt());
+        // Nog doen: koppeling met MetaData klasse?
+        String optGebruiker =
+            gOpt.getOpt() + ":" + cmd.getOptionValue(gOpt.getValue());
+        String optSchool =
+            sOpt.getOpt() + ":" + cmd.getOptionValue(sOpt.getValue());
+        String optVakleergebied =
+            vOpt.getOpt() + ":" + cmd.getOptionValue(vOpt.getValue());
+        String optDidactischeFunctie =
+            dOpt.getOpt() + ":" + cmd.getOptionValue(dOpt.getValue());
+        String optProducttype =
+            pOpt.getOpt() + ":" + cmd.getOptionValue(pOpt.getValue());
+        String optBeroepsituatie =
+            bOpt.getOpt() + ":" + cmd.getOptionValue(bOpt.getValue());
+        String optCompetentie =
+            cOpt.getOpt() + ":" + cmd.getOptionValue(cOpt.getValue());
+        String optTrefwoorden = cmd.getOptionValue(tOpt.getValue());
 
-        if (optIdentity != null && optSchool != null && optSearch != null)
-            performSearch(optIdentity, optSchool, optSearch);
-        else {
-            HelpFormatter f = new HelpFormatter();
-            f.printHelp("ApplicatieZ", options);
+        String searchString =
+            optGebruiker + optSchool + optVakleergebied + optVakleergebied +
+            optDidactischeFunctie + optProducttype + optBeroepsituatie +
+            optCompetentie;
+
+        if (checkQuery(searchString))
+            performSearch(searchString);
+        //   else {
+        //       HelpFormatter f = new HelpFormatter();
+        //       f.printHelp("ApplicatieZ", options);
+    }
+
+    public static void performSearch(String searchString) {
+        System.out.println("Here are the results of the Meta/Z jury:");
+        System.out.println();
+        SearchServiceImpl searchServiceImpl = new SearchServiceImpl();
+        if (checkQuery(searchString)) {
+            List list = searchServiceImpl.doSearch("een test");
+            if (list != null)
+                // link naar repository werkt nog niet...
+                for (int i = 0; i < list.size(); i++)
+                    System.out.println(list.toString());
+
         }
     }
 
-    public static void performSearch(String Identity, String schoolType,
-                                     String searchString) {
+    /**
+     * Checks whether the commandline input is a valid searchString.
+     * Query ::= (FullTextSearchString) | (Clause)*
+     * Clause ::= <FIELDNAME>:<VALUE>
+     * @param searchString
+     * @return boolean
+     */
+    public static boolean checkQuery(String searchString) {
 
-        System.out.println("Here are the results of the Meta/Z jury:");
-        System.out.println();
-        System.out.println("...we don't know yet...");
+        return true;
     }
+
+
 }
