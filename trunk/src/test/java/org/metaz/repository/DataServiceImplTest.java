b984dd72dde1cde64c6897240ec5898b4a971c25
ejspaans <ejspaans@fa9a27b5-f702-0410-86e9-f74c14d606bd>
2006-01-29 21:47:12 +0000

    Initial version of test for DataServiceImpl

---
+++ trunk/src/test/java/org/metaz/repository/DataServiceImplTest.java
@@ -0,0 +1,68 @@
+package org.metaz.repository;
+
+import junit.framework.TestCase;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.metaz.domain.BooleanMetaData;
+import org.metaz.domain.HyperlinkMetaData;
+import org.metaz.domain.MetaData;
+import org.metaz.domain.Record;
+import org.metaz.domain.TextMetaData;
+
+public class DataServiceImplTest extends TestCase {
+	
+	private static Logger LOG = Logger.getLogger(DataServiceImplTest.class);
+
+	/*
+	 * Test method for 'org.metaz.repository.DataServiceImpl.getRecords(List<URI>)'
+	 */
+	public void testGetRecords() {
+		BasicConfigurator.configure();
+		LOG.debug("Testing");
+		TextMetaData title = new TextMetaData();
+		title.setName("titel");
+		title.setValue("someValue");
+		title.setMandatory(true);
+
+		BooleanMetaData secured = new BooleanMetaData();
+		secured.setName("beveiligd");
+		secured.setValue(false);
+		secured.setMandatory(true);
+
+		TextMetaData fileFormat = new TextMetaData();
+		fileFormat.setName("bestandsformaat");
+		fileFormat.setValue("application/pdf");
+		fileFormat.setMandatory(true);
+
+		TextMetaData didacticalFunction = new TextMetaData();
+		didacticalFunction.setName("didactischeFunctie");
+		didacticalFunction.setValue("bla");
+		didacticalFunction.setMandatory(true);
+
+		TextMetaData productType = new TextMetaData();
+		productType.setName("producttype");
+		productType.setValue("instructie");
+		productType.setMandatory(true);
+
+		HyperlinkMetaData uri = new HyperlinkMetaData();
+		uri.setName("URI");
+		uri.setValue("http://www.ou.nl/stories/ruuddemoor.pdf");
+
+		Record rec = new Record(title, secured, fileFormat, didacticalFunction,
+				productType, uri);
+		
+		Configuration cfg = new Configuration().configure("org/metaz/hibernate.cfg.xml");
+		SessionFactory sf = cfg.buildSessionFactory();
+		Session sess = sf.openSession();
+		Transaction t = sess.beginTransaction();
+		sess.save(rec);
+		t.commit();
+		sess.close();
+	}
+
+}
