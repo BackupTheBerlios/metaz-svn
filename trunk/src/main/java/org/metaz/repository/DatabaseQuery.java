eddd4eb48428498236571b54c66db134124083f9
ejspaans <ejspaans@fa9a27b5-f702-0410-86e9-f74c14d606bd>
2006-02-01 20:37:51 +0000

    Moved example files to example package.

--- trunk/src/main/java/org/metaz/repository/DatabaseQuery.java
+++
@@ -1,9 +0,0 @@
-// This is an abstract class that defines an abstract "query"
-// Independent from persistence technologies such as Hibernate, etc
-// @author Falco Paul
-
-package org.metaz.repository;
-
-public interface DatabaseQuery extends DatabaseInteraction {
-  
-}
