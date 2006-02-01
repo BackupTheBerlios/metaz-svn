c881835ccf8b20c62cb97d0421e96fe8eb55c569
erik-metaz <erik-metaz@fa9a27b5-f702-0410-86e9-f74c14d606bd>
2006-02-01 21:19:38 +0000

    herschikking methodes

--- trunk/src/main/java/org/metaz/gui/PrototypeCLI.java
+++ trunk/src/main/java/org/metaz/gui/PrototypeCLI.java
@@ -10,12 +10,13 @@ import org.apache.commons.cli.Options;
 
 import org.metaz.domain.MetaData;
 import org.metaz.repository.Facade;
-import org.metaz.repository.FacadeFactory;
+import org.metaz.util.MetaZ;
 
 
 public class PrototypeCLI {
 
-private static String syntax = "PrototypeCLI -option [goal] [[-option goal][..]";
+    private static String syntax =
+        "PrototypeCLI -option [goal] [[-option goal][..]";
 
     public PrototypeCLI() {
     }
@@ -23,6 +24,77 @@ private static String syntax = "PrototypeCLI -option [goal] [[-option goal][..]"
     public static void main(String[] args) {
         //PrototypeCLI prototypeCLI = new PrototypeCLI();
 
+        // Definition of the possible CLI options
+        Options options = defineOptions();
+
+        BasicParser parser = new BasicParser();
+        CommandLine cmd = null;
+
+        // Reading of the input
+        try {
+            cmd = parser.parse(options, args);
+            if (cmd.hasOption("h")) {
+                HelpFormatter f = new HelpFormatter();
+                System.out.println();
+                f.printHelp(syntax, options);
+                java.lang.System.exit(0);
+            }
+        } catch (Exception e) {
+            System.out.println();
+            System.out.println("Command line option parsing failed");
+            System.out.println("Option error: " + e.toString());
+            System.out.println();
+            HelpFormatter f = new HelpFormatter();
+            f.printHelp(syntax, options);
+            java.lang.System.exit(0);
+        }
+
+        // The searchString should have the correct syntax.
+        String searchString = printClauses(cmd, options);
+
+        // If all is right, send the searchString to the searchService.
+        if (checkQuery(searchString))
+            performSearch(searchString);
+        else
+            System.out.println("fout in checkQuery");
+    }
+
+    /**
+     * <p> Sends the searchstring to the searchservice through the MetaZ 
+     * singleton instance method getRepositoryFacade().
+     * @param searchString
+     */
+    public static void performSearch(String searchString) {
+        System.out.println();
+
+        Facade myFacade = MetaZ.getInstance().getRepositoryFacade();
+        try {
+            List list = myFacade.doSearch(searchString);
+            System.out.println(list.toString());
+        } catch (Exception e) {
+            // TODO
+            System.out.println("niet gelukt");
+        }
+    }
+
+    /**
+     * <p>Checks whether the commandline input is a valid searchString.<br>
+     * <code>Query ::= (Clause)<sup>+</sup><br>
+     * Clause ::= (FullTextSearchString) | &lt;FIELDNAME&gt;:&lt;VALUE&gt;<br></code>
+     * @param searchString
+     * @return boolean
+     */
+    public static boolean checkQuery(String searchString) {
+        if (searchString == "")
+            return false;
+        return true;
+    }
+
+    /**
+     * Defines the possible CLI options
+     * @return Options
+     */
+    private static Options defineOptions() {
         // Definition of the CLI options
         Options options = new Options();
 
@@ -88,126 +160,75 @@ private static String syntax = "PrototypeCLI -option [goal] [[-option goal][..]"
         hOpt.setRequired(false);
         options.addOption(hOpt);
 
-        BasicParser parser = new BasicParser();
-        CommandLine cmd = null;
-
-        // Reading of the input
-        try {
-            cmd = parser.parse(options, args);
-            if (cmd.hasOption("h")) {
-                HelpFormatter f = new HelpFormatter();
-                System.out.println();                
-                f.printHelp(syntax, options);
-                java.lang.System.exit(0);
-            }
-        } catch (Exception e) {
-            System.out.println();
-            System.out.println("Command line option parsing failed");
-            System.out.println("Option error: " + e.toString());
-            System.out.println();
-            HelpFormatter f = new HelpFormatter();
-            f.printHelp(syntax, options);
-            java.lang.System.exit(0);
-        }
-
-        // Ordering of the CLI options
-        String[] shortOpts = new String[options.getOptions().size()];
-        // Onderstaande kan waarschijnlijk efficienter...
-        int j = 0;
-        if (cmd.hasOption("t")) {
-            shortOpts[j] = "t";
-            j++;
-        }
-        if (cmd.hasOption("g")) {
-            shortOpts[j] = "g";
-            j++;
-        }
-        if (cmd.hasOption("s")) {
-            shortOpts[j] = "s";
-            j++;
-        }
-        if (cmd.hasOption("v")) {
-            shortOpts[j] = "v";
-            j++;
-        }
-        if (cmd.hasOption("d")) {
-            shortOpts[j] = "d";
-            j++;
-        }
-        if (cmd.hasOption("p")) {
-            shortOpts[j] = "p";
-            j++;
-        }
-        if (cmd.hasOption("b")) {
-            shortOpts[j] = "b";
-            j++;
-        }
-        if (cmd.hasOption("c")) {
-            shortOpts[j] = "c";
-            j++;
-        }
-        
-        // The searchString given to the searchservice should have the correct syntax.
-        String searchString = printClauses(cmd, options, shortOpts);
-
-        // If all is right, send the searchString to the searchService.
-        if (checkQuery(searchString))
-            performSearch(searchString);
-        else
-            System.out.println("fout in checkQuery");
-    }
-
-
-    public static void performSearch(String searchString) {
-        System.out.println();
-        Facade myFacade = FacadeFactory.createFacade();
-        try {
-            List list = myFacade.doSearch(searchString);
-            System.out.println(list.toString());
-        } catch (Exception e) {
-            // TODO
-            System.out.println("niet gelukt");
-        }
+        return options;
     }
 
     /**
-     * <p>Checks whether the commandline input is a valid searchString.<br>
-     * <code>Query ::= (FullTextSearchString) | (Clause)*<br>
-     * Clause ::= &lt;FIELDNAME&gt;:&lt;VALUE&gt;<br></code>
-     * @param searchString
-     * @return boolean
+     * <p> Prints a string containing all the clauses. 
+     * This is necessary for Lucene.
+     * @param cl
+     * @param options
+     * @return String
      */
-    public static boolean checkQuery(String searchString) {
-        if (searchString == "")
-            return false;
-        return true;
-    }
-
-    private static String printClauses(CommandLine cl, Options options,
-                                        String[] opts) {
+    private static String printClauses(CommandLine cl, Options options) {
         String clausule = "";
         Option o;
         int j = 0;
 
+        // Ordering of the CLI options
+        String[] shortOpts = new String[options.getOptions().size()];
+        // Onderstaande kan waarschijnlijk efficienter...
+        int k = 0;
+        if (cl.hasOption("t")) {
+            shortOpts[k] = "t";
+            k++;
+        }
+        if (cl.hasOption("g")) {
+            shortOpts[k] = "g";
+            k++;
+        }
+        if (cl.hasOption("s")) {
+            shortOpts[k] = "s";
+            k++;
+        }
+        if (cl.hasOption("v")) {
+            shortOpts[k] = "v";
+            k++;
+        }
+        if (cl.hasOption("d")) {
+            shortOpts[k] = "d";
+            k++;
+        }
+        if (cl.hasOption("p")) {
+            shortOpts[k] = "p";
+            k++;
+        }
+        if (cl.hasOption("b")) {
+            shortOpts[k] = "b";
+            k++;
+        }
+        if (cl.hasOption("c")) {
+            shortOpts[k] = "c";
+        }
+
         // the freetext searchstring
-        if (opts[j] == "t") {
-            o = options.getOption(opts[0]);
-            String[] keywords = cl.getOptionValues(opts[j]);
-            String optTrefwoorden = "";
+        if (shortOpts[j] == "t") {
+            o = options.getOption(shortOpts[0]);
+            String[] keywords = cl.getOptionValues(shortOpts[j]);
             for (int i = 0; i < keywords.length; i++)
-                clausule += keywords[i] + " ";            
+                clausule += keywords[i] + " ";
             j = 1;
         }
 
         // metadata valuepairs
-        for (int i = j; ((opts[i] != null) && (i < opts.length)); i++) {
-            o = options.getOption(opts[i]);
+        for (int i = j; ((shortOpts[i] != null) && (i < shortOpts.length));
+             i++) {
+            o = options.getOption(shortOpts[i]);
             clausule +=
-                o.getLongOpt() + ":" + cl.getOptionValue(opts[i]) +
-                " ";
+                o.getLongOpt() + ":" + cl.getOptionValue(shortOpts[i]) + " ";
         }
         System.out.println();
-        System.out.println("ZOEKSTRING: "+clausule);
+        System.out.println("ZOEKSTRING: " + clausule);
         return clausule;
     }
 
