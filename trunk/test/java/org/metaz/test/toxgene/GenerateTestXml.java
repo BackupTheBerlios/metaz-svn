57029a10f1513e908a1c81724f0c5d68dabb75a8
sdalewyn <sdalewyn@fa9a27b5-f702-0410-86e9-f74c14d606bd>
2006-03-06 13:25:31 +0000

    changed ToXgene templates according to new schooltype value list; jalopy

--- trunk/test/java/org/metaz/test/toxgene/GenerateTestXml.java
+++ trunk/test/java/org/metaz/test/toxgene/GenerateTestXml.java
@@ -21,262 +21,275 @@ import java.text.DecimalFormat;
 
 import java.util.Vector;
 
+/**
+ * Generates a test xml file. The formatting is done in the template file.
+ *
+ * @author Sammy Dalewyn
+ * @version 1.0
+ */
+public class GenerateTestXml {
+
+  //~ Static fields/initializers ---------------------------------------------------------------------------------------
+
+  private static Engine              tgEngine;
+  private static ToXgeneReporterImpl tgReporter;
+  private static String              template;
+  private static MetaZ               app;
+  private static Vector              descriptors;
+
+  //~ Constructors -----------------------------------------------------------------------------------------------------
 
 /**
- * Generates a test xml file.
- * The formatting is done in the template file.
- *
- * @author Sammy Dalewyn    
- * @version 1.0
-  */
-public class GenerateTestXml
-{
-    private static Engine tgEngine;
-    private static ToXgeneReporterImpl tgReporter;
-    private static String template;
-    private static MetaZ app;
-    private static Vector descriptors;
-
-    /**
      * Creates a new GenerateTestXml object.
      */
-    public GenerateTestXml()
-    {
-    } // end GenerateTestXml()
+  public GenerateTestXml() {
 
-    /**
-     * The main function
-     *
-     * @param argv none
-     */
-    public static void main(String[] argv)
-    {
-        if (argv.length == 0) {
-            System.out.println("Usage: GenerateTestXml <template file>\n");
-            System.exit(0);
-        }
-        try
-        {
-            //template = "test/toxgene/records.tsl";
-            template = argv[0];
-            app = MetaZ.getInstance();
+  } // end GenerateTestXml()
 
-            ToXgeneCdataDescriptor dutchDescriptor = new DutchWordsCdataDescriptor();
-            descriptors = new Vector();
-            descriptors.add(dutchDescriptor);
+  //~ Methods ----------------------------------------------------------------------------------------------------------
 
-            ToXgeneCdataDescriptor gibberishDescr = new ToXgeneCdataDescriptor();
-            gibberishDescr.cdataClass = "toxgene.core.genes.literals.ToxString";
-            gibberishDescr.cdataName = "gibberish";
-            gibberishDescr.minLength = 0;
-            gibberishDescr.maxLength = 200;
-            descriptors.add(gibberishDescr);
+  /**
+   * The main function
+   *
+   * @param argv none
+   */
+  public static void main(String[] argv) {
 
-            tgEngine = new Engine();
+    if (argv.length == 0) {
 
-            boolean verbose = true; /* useful for debugging
-              * templates */
+      System.out.println("Usage: GenerateTestXml <template file>\n");
+      System.exit(0);
 
-            boolean showWarnings = true;
-            tgReporter = new ToXgeneReporterImpl(verbose, showWarnings);
+    }
+
+    try {
+
+      //template = "test/toxgene/records.tsl";
+      template = argv[0];
+      app = MetaZ.getInstance();
+
+      ToXgeneCdataDescriptor dutchDescriptor = new DutchWordsCdataDescriptor();
+
+      descriptors = new Vector();
+      descriptors.add(dutchDescriptor);
+
+      ToXgeneCdataDescriptor gibberishDescr = new ToXgeneCdataDescriptor();
+
+      gibberishDescr.cdataClass = "toxgene.core.genes.literals.ToxString";
+      gibberishDescr.cdataName = "gibberish";
+      gibberishDescr.minLength = 0;
+      gibberishDescr.maxLength = 200;
+      descriptors.add(gibberishDescr);
+
+      tgEngine = new Engine();
+
+      boolean verbose = true; /* useful for debugging
+       * templates */
+
+      boolean showWarnings = true;
+
+      tgReporter = new ToXgeneReporterImpl(verbose, showWarnings);
+
+      /*
+       * The ToXgeneSession specifies all parameters the
+       * engine needs for generating the documents.
+       */
+      ToXgeneSession session = new ToXgeneSession();
+
+      session.reporter = tgReporter;
+      session.initialSeed = 123456;
+      session.addNewLines = true;
+      session.inputPath = "./";
+      session.usePOM = false;
+      session.pomBufferPath = ".";
+      session.pomMemFracBuffer = (float) 0.5;
+      session.pomBufferSize = 8 * 1024;
+      session.cdataDescriptors = descriptors;
+
+      /* Initialize the engine */
+      tgEngine.startSession(session);
+
+      /*
+       * The progress() method sends a progress report
+       * message to the message handler.
+       */
+      tgReporter.progress("Parsing template: ");
+
+      File f = app.getRelativeFile(template);
+
+      tgEngine.parseTemplate(new FileInputStream(f));
+      tgReporter.progress("Done !\n");
+
+      /*
+       * The generateLists() method tells the engine to
+       * generate all temporary data declared in tox-list
+       * elements in the template. Calling this method is
+       * optional, ToXgene will materialize all temporary
+       * data if needed even if generateLists is not
+       * invoked.
+       */
+      tgEngine.generateLists();
+      generateCollections();
+
+    } // end try
+    catch (ToXgeneErrorException e1) {
+
+      error(e1.getMessage());
+
+    } // end catch
+    catch (FileNotFoundException e) {
+
+      tgEngine.endSession();
+      error("cannot open template file \"" + template + "\"");
+
+    } // end catch
+    catch (Exception e) {
+
+      e.printStackTrace();
+
+    } // end catch
+
+    tgEngine.endSession();
+
+    int nWarnings = tgReporter.warnings();
+
+    if (nWarnings > 0) {
+
+      System.out.println("There were " + nWarnings + " warning(s).");
+      tgReporter.printAllWarnings();
+
+    } // end if
+
+    System.exit(0);
+
+  } // end main()
+
+  /**
+   * Scans the collections declared in the template and generates the XML documents they specify on files.
+   *
+   * @throws ToXgeneErrorException when the xml document can not be generated
+   */
+  private static void generateCollections() {
+
+    Vector collections = tgEngine.getToXgeneDocumentCollections();
+    String outputPath = "./"; /* Path where to put the
+     * documents */
+
+    if (collections.size() == 0) {
+
+      tgReporter.warning("no document genes found");
+
+      return;
+
+    } // end if
+
+    /* Iterate over all collections in the template */
+    for (int i = 0; i < collections.size(); i++) {
+
+      ToXgeneDocumentCollection tgColl = (ToXgeneDocumentCollection) collections.get(i);
+
+      /*
+       * Test whether this collection has more than one
+       * document
+       */
+      if (tgColl.getSize() > 1) {
+
+        int           start = tgColl.getStartingNumber();
+        int           documents = tgColl.getSize();
+        DecimalFormat nf = new DecimalFormat("0;0");
+
+        tgReporter.progress("Generating collection: " + tgColl.getName());
+
+        String current;
+        int    count = 0;
+        int    sum = 0;
+
+        for (int j = start; j < (start + documents); j++) {
+
+          current = outputPath + tgColl.getName() + nf.format(j) + ".xml";
+
+          try {
 
             /*
-             *
-             * if (System.getProperty("TOXGENE_HOME") == null)
-             * {
-             *   System.out.println("\n***** WARNING: " +
-             *       "TOXGENE_HOME property is not set. " +
-             *       "ToXgene will attempt to load\n" +
-             *       "toxgene.jar/config/cdata.xml assuming" +
-             *       "toxgene.jar is in the current " +
-             *       "directory.\n\nUse java " + "-DToXgene_home=<path>... " +
-             *       "to override this.");
-             *} // end if
+             * Create a file for storing
+             * this document; note that
+             * any PrintStrem object
+             * would work here as far as
+             * ToXgene is concerned.
              */
+            PrintStream outStream = new PrintStream(new FileOutputStream(current));
 
             /*
-             * The ToXgeneSession specifies all parameters the
-             * engine needs for generating the documents.
+             * The materialize() method
+             * "prints" the document into
+             * the given PrintStream
+             * object.
              */
-            ToXgeneSession session = new ToXgeneSession();
-            session.reporter = tgReporter;
-            session.initialSeed = 123456;
-            session.addNewLines = true;
-            session.inputPath = "./";
-            session.usePOM = false;
-            session.pomBufferPath = ".";
-            session.pomMemFracBuffer = (float) 0.5;
-            session.pomBufferSize = 8 * 1024;
-            session.cdataDescriptors = descriptors;
+            tgEngine.materialize(tgColl, outStream);
 
-            /* Initialize the engine */
-            tgEngine.startSession(session);
+          } // end try
+          catch (Exception e) {
 
             /*
-             * The progress() method sends a progress report
-             * message to the message handler.
+             * The endSession() method
+             * tells ToXgene's engine to
+             * clean up, e.g., temporary
+             * files it may have created.
              */
-            tgReporter.progress("Parsing template: ");
-
-            File f = app.getRelativeFile(template);
-            tgEngine.parseTemplate(new FileInputStream(f));
-            tgReporter.progress("Done !\n");
-
-            /*
-             * The generateLists() method tells the engine to
-             * generate all temporary data declared in tox-list
-             * elements in the template. Calling this method is
-             * optional, ToXgene will materialize all temporary
-             * data if needed even if generateLists is not
-             * invoked.
-             */
-            tgEngine.generateLists();
-            generateCollections();
-        } // end try
-        catch (ToXgeneErrorException e1)
-        {
-            error(e1.getMessage());
-        } // end catch
-        catch (FileNotFoundException e)
-        {
             tgEngine.endSession();
-            error("cannot open template file \"" + template + "\"");
-        } // end catch
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        } // end catch
+            error("Couldn't create " + current);
 
-        tgEngine.endSession();
+          } // end catch
 
-        int nWarnings = tgReporter.warnings();
-
-        if (nWarnings > 0)
-        {
-            System.out.println("There were " + nWarnings + " warning(s).");
-            tgReporter.printAllWarnings();
-        } // end if
-
-        System.exit(0);
-    } // end main()
-
-    /**
-     * Scans the collections declared in the template and generates the
-     * XML documents they specify on files.
-     *
-     * @throws ToXgeneErrorException when the xml document can not be generated
-     */
-    private static void generateCollections()
-    {
-        Vector collections = tgEngine.getToXgeneDocumentCollections();
-        String outputPath = "./"; /* Path where to put the
-         * documents */
-
-        if (collections.size() == 0)
-        {
-            tgReporter.warning("no document genes found");
-
-            return;
-        } // end if
-
-        /* Iterate over all collections in the template */
-        for (int i = 0; i < collections.size(); i++)
-        {
-            ToXgeneDocumentCollection tgColl = (ToXgeneDocumentCollection) collections.get(i);
-
-            /*
-             * Test whether this collection has more than one
-             * document
-             */
-            if (tgColl.getSize() > 1)
-            {
-                int start = tgColl.getStartingNumber();
-                int documents = tgColl.getSize();
-                DecimalFormat nf = new DecimalFormat("0;0");
-
-                tgReporter.progress("Generating collection: " +
-                    tgColl.getName());
-
-                String current;
-                int count = 0;
-                int sum = 0;
-
-                for (int j = start; j < (start + documents); j++)
-                {
-                    current = outputPath + tgColl.getName() + nf.format(j) +
-                        ".xml";
-
-                    try
-                    {
-                        /*
-                         * Create a file for storing
-                         * this document; note that
-                         * any PrintStrem object
-                         * would work here as far as
-                         * ToXgene is concerned.
-                         */
-                        PrintStream outStream = new PrintStream(new FileOutputStream(
-                                    current));
-                        /*
-                         * The materialize() method
-                         * "prints" the document into
-                         * the given PrintStream
-                         * object.
-                         */
-                        tgEngine.materialize(tgColl, outStream);
-                    } // end try
-                    catch (Exception e)
-                    {
-                        /*
-                         * The endSession() method
-                         * tells ToXgene's engine to
-                         * clean up, e.g., temporary
-                         * files it may have created.
-                         */
-                        tgEngine.endSession();
-                        error("Couldn't create " + current);
-                    } // end catch
-                } // end for
-
-                tgReporter.progress(" ...Done!\n");
-            } // end if
-            else
-            {
-                /*
-                 * In this case, the collection has a single
-                 * document
-                 */
-                tgReporter.progress("Generating document \"" +
-                    tgColl.getName() + ".xml\"");
-
-                String file = outputPath + tgColl.getName() + ".xml";
-
-                try
-                {
-                    //PrintStream outStream = new PrintStream(new FileOutputStream(file));
-                    PrintStream outStream = new PrintStream(new FileOutputStream(file),true,"US-ASCII");
-                    tgEngine.materialize(tgColl, outStream);
-                } // end try
-                catch (Exception e)
-                {
-                    tgEngine.endSession();
-                    throw new ToXgeneErrorException("Couldn't create " + file);
-                } // end catch
-
-                tgReporter.progress(" ...Done!\n");
-            } // end else
         } // end for
-    } // end generateCollections()
 
-    /**
-     * Prints an error message and aborts.
-     *
-     * @param msg the error message
-     */
-    public static void error(String msg)
-    {
-        tgReporter.printAllWarnings();
-        System.out.println("\n***** ERROR: " + msg);
-        System.exit(1);
-    } // end error()
+        tgReporter.progress(" ...Done!\n");
+
+      } // end if
+      else {
+
+        /*
+         * In this case, the collection has a single
+         * document
+         */
+        tgReporter.progress("Generating document \"" + tgColl.getName() + ".xml\"");
+
+        String file = outputPath + tgColl.getName() + ".xml";
+
+        try {
+
+          //PrintStream outStream = new PrintStream(new FileOutputStream(file));
+          PrintStream outStream = new PrintStream(new FileOutputStream(file), true, "US-ASCII");
+
+          tgEngine.materialize(tgColl, outStream);
+
+        } // end try
+        catch (Exception e) {
+
+          tgEngine.endSession();
+          throw new ToXgeneErrorException("Couldn't create " + file);
+
+        } // end catch
+
+        tgReporter.progress(" ...Done!\n");
+
+      } // end else
+
+    } // end for
+
+  } // end generateCollections()
+
+  /**
+   * Prints an error message and aborts.
+   *
+   * @param msg the error message
+   */
+  public static void error(String msg) {
+
+    tgReporter.printAllWarnings();
+    System.out.println("\n***** ERROR: " + msg);
+    System.exit(1);
+
+  } // end error()
+
 } // end GenerateTestXml
--- trunk/test/java/org/metaz/test/toxgene/Tekst.java
+++ trunk/test/java/org/metaz/test/toxgene/Tekst.java
@@ -13,118 +13,137 @@ import java.io.InputStreamReader;
 
 import java.util.Random;
 
-
 /**
- * This class implements a simple cdata generator that produces random
- * dutch text.
+ * This class implements a simple cdata generator that produces random dutch text.
  *
- * @author Sammy Dalewyn  
+ * @author Sammy Dalewyn
  * @version 1.0
-  */
-public class Tekst implements ToXgeneCdataGenerator
+ */
+public class Tekst
+  implements ToXgeneCdataGenerator
 {
-    private final static int len = 45669;
-    private static String[] data;
-    private static boolean loaded = false;
-    private static Random pick;
-    private static String result;
-    private static MetaZ app;
-    private static String wordlist = "test/toxgene/input/voc.txt";
+
+  //~ Static fields/initializers ---------------------------------------------------------------------------------------
+
+  private final static int len = 45669;
+  private static String[]  data;
+  private static boolean   loaded = false;
+  private static Random    pick;
+  private static String    result;
+  private static MetaZ     app;
+  private static String    wordlist = "test/toxgene/input/voc.txt";
+
+  //~ Constructors -----------------------------------------------------------------------------------------------------
 
 /**
      * Constructor. There are too many words to be loaded here, so
      * we cannot declare them explicitly as data = {...}, so we load the
      * words stored in an external text file.
      */
-    public Tekst()
-    {
-        init();
-    } // end Tekst()
+  public Tekst() {
 
-    /**
-     * Specifies a seed for the random generator so that repeated
-     * executions always produce the same content, if the same seed is
-     * provided.
-     *
-     * @param seed random seed to initialize the random generator
-     */
-    public void setRandomSeed(int seed)
-    {
-        pick = new Random(seed);
-    } // end setRandomSeed()
+    init();
 
-    /**
-     * Generates random dutch text that consists of at most wordCount words.
-     *
-     * @param wordCount the number of words
-     *
-     * @return a dutch words text string
-     */
-    public String getCdata(int wordCount)
-    {
-        //length == -1 means that the string should not be trimmed
-        if (wordCount == -1)
-        {
-            wordCount = (int) (1024.0 * Math.random());
-        } // end if
+  } // end Tekst()
 
-        if (wordCount == 0)
-        {
-            wordCount = 1;
-        } // end if
+  //~ Methods ----------------------------------------------------------------------------------------------------------
 
-        result = new String();
+  /**
+   * Specifies a seed for the random generator so that repeated executions always produce the same content, if
+   * the same seed is provided.
+   *
+   * @param seed random seed to initialize the random generator
+   */
+  public void setRandomSeed(int seed) {
 
-        for (int i = 0; i < wordCount; i++)
-        {
-            result += (Tekst.data[(int) pick.nextInt(len)] + " ");
-        } // end for
+    pick = new Random(seed);
 
-        return result;
-    } // end getCdata()
+  } // end setRandomSeed()
 
-    /**
-     * This method does all the work of loading the words into memory.
-     */
-    public static void init()
-    {
-        if (loaded)
-        {
-            return;
+  /**
+   * Generates random dutch text that consists of at most wordCount words.
+   *
+   * @param wordCount the number of words
+   *
+   * @return a dutch words text string
+   */
+  public String getCdata(int wordCount) {
+
+    //length == -1 means that the string should not be trimmed
+    if (wordCount == -1) {
+
+      wordCount = (int) (1024.0 * Math.random());
+
+    } // end if
+
+    if (wordCount == 0) {
+
+      wordCount = 1;
+
+    } // end if
+
+    result = new String();
+
+    for (int i = 0; i < wordCount; i++) {
+
+      result += (Tekst.data[(int) pick.nextInt(len)] + " ");
+
+    } // end for
+
+    return result;
+
+  } // end getCdata()
+
+  /**
+   * This method does all the work of loading the words into memory.
+   */
+  public static void init() {
+
+    if (loaded) {
+
+      return;
+
+    }
+
+    data = new String[len];
+
+    try {
+
+      app = MetaZ.getInstance();
+
+      File            f = app.getRelativeFile(wordlist);
+      FileInputStream fs = new FileInputStream(f);
+      BufferedReader  reader = new BufferedReader(new InputStreamReader(fs));
+
+      String line = null;
+      int    count;
+
+      count = 0;
+
+      do {
+
+        line = reader.readLine();
+
+        if (line == null) {
+
+          break;
+
         }
 
-        data = new String[len];
+        data[count++] = line;
 
-        try
-        {
-            app = MetaZ.getInstance();
+      } // end do
+       while (true);
 
-            File f = app.getRelativeFile(wordlist);
-            FileInputStream fs = new FileInputStream(f);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(fs));
+    } // end try
+    catch (Exception e) {
 
-            String line = null;
-            int count;
-            count = 0;
+      ToXgene.error("could not initialize text generator (tekst.txt)");
 
-            do
-            {
-                line = reader.readLine();
+    } // end catch
 
-                if (line == null)
-                {
-                    break;
-                }
+    loaded = true;
 
-                data[count++] = line;
-            } // end do
-            while (true);
-        } // end try
-        catch (Exception e)
-        {
-            ToXgene.error("could not initialize text generator (tekst.txt)");
-        } // end catch
+  } // end init()
 
-        loaded = true;
-    } // end init()
 } // end Tekst
