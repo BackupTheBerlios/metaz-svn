868af1fc68ed4960cc6e8c77b03460b5e7498c2c
loosterloo <loosterloo@fa9a27b5-f702-0410-86e9-f74c14d606bd>
2006-03-12 17:13:21 +0000

    after jalopy

--- trunk/src/main/java/org/metaz/domain/RecordAttributeSetter.java
+++ trunk/src/main/java/org/metaz/domain/RecordAttributeSetter.java
@@ -5,13 +5,12 @@ import java.util.List;
 import java.util.Vector;
 
 /**
- * The class RecordAttributeSetter is used to collect attributes around record metadata attributes so 
- * it can be used to iterate through the collection of record attributes
- * and retrieve metadata on the attributes
+ * The class RecordAttributeSetter is used to collect attributes around record metadata attributes so  it can be
+ * used to iterate through the collection of record attributes and retrieve metadata on the attributes
  *
  * @author Lars Oosterloo
  * @version 1.0
-  */
+ */
 public class RecordAttributeSetter {
 
   //~ Instance fields --------------------------------------------------------------------------------------------------
@@ -62,8 +61,7 @@ public class RecordAttributeSetter {
   //~ Methods ----------------------------------------------------------------------------------------------------------
 
   /**
-   * 
-  DOCUMENT ME!
+   * DOCUMENT ME!
    *
    * @param args
    */
@@ -80,6 +78,7 @@ public class RecordAttributeSetter {
   public Iterator iterator() {
 
     if (recordMetadata.isEmpty()) {
+
       title = new TextMetaData();
       title.setMandatory(true);
       title.setXMLTagName(MetaData.TITLE);
@@ -236,6 +235,7 @@ public class RecordAttributeSetter {
       roleName.setMetaDataType("TextMetaData");
       roleName.setOptional(true);
       recordMetadata.add(roleName);
+
     } // end if
 
     return recordMetadata.iterator();
